4ec0b54d3e4afd6204caf5d292d6aae8960ef0d2
Sagar Anand <saagarannand@outlook.com>
2025-05-17 17:59:04 +0530

    Update db.js

--- Dynamic-QR-Attendance-System-REST-API/Config/db.js
+++ Dynamic-QR-Attendance-System-REST-API/Config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://itzshubhamofficial:Shubham%401166@qrcluster.rh3p7.mongodb.net/?retryWrites=true&w=majority&appName=QRCLUSTER/QR_Attendance");
+        await mongoose.connect("DB_Connection_link");
         console.log("Database Connected");
     } catch (error) {
         console.error("Database connection error:", error);
